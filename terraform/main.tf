6c281af9976749a995f866dd21c3563f0f5e664f
CyuanT <tan.yuan.cy@gmail.com>
2024-10-30 20:44:02 +0800

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,96 +1,22 @@
-# resource "aws_s3_bucket" "ci-example" {
-#     bucket = "ce7-ty-terraform-ci-bucket"
+terraform {
+  required_version = ">= 1.6"
 
-#     versioning_configuration {
-#     status = "Enabled"
-#   }
-#     lifecycle_rule {
-#     id      = "expire"
-#     status  = "Enabled"
-#     prefix  = "logs/"
-#     transition {
-#       days          = 30
-#       storage_class = "STANDARD_IA"
-#     }
-#     expiration {
-#       days = 90
-#     }
-#   }
-# }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-# resource "aws_sns_topic" "bucket_notifications" {
-#   name = "ce7-ty-ci-notifications"
-#   kms_master_key_id = "alias/aws/sns"
-# }
-
-# resource "aws_s3_bucket_notification" "bucket_notification" {
-#   bucket = aws_s3_bucket.ci-example.id
-
-#   topic {
-#     topic_arn     = aws_sns_topic.bucket_notifications.arn
-#     events        = ["s3:ObjectCreated:*"]
-#     filter_prefix = "logs/"
-#   }
-# }
-
-# resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
-#   restrict_public_buckets = true
-#   bucket = aws_s3_bucket.ci-example.id
-#   block_public_acls   = true
-#   block_public_policy = true
-#   ignore_public_acls=true
-# }
-
-# resource "aws_s3_bucket_versioning" "east" {
-#   bucket = aws_s3_bucket.ci-example.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
-# resource "aws_s3_bucket" "west" {
-#   provider = aws.west
-#   bucket   = "ce7-ty-ci-bucket-west"
-# }
-
-# resource "aws_s3_bucket_versioning" "west" {
-#   provider = aws.west
-
-#   bucket = aws_s3_bucket.west.id
-#   versioning_configuration {
-#     status = "Enabled"
-#   }
-# }
-
-# resource "aws_s3_bucket_replication_configuration" "east_to_west" {
-#   depends_on = [aws_s3_bucket_versioning.east]
-#   role   = aws_iam_role.east_replication.arn
-#   bucket = aws_s3_bucket.east.id
-
-#   rule {
-#     status = "Enabled"
-
-#     destination {
-#       bucket        = aws_s3_bucket.west.arn
-#       storage_class = "STANDARD"
-#     }
-#   }
-# }
-
-# resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
-#   bucket = aws_s3_bucket.ci-example.bucket
-
-#   rule {
-#     apply_server_side_encryption_by_default {
-#       kms_master_key_id = aws_kms_key.mykey.arn
-#       sse_algorithm     = "aws:kms"
-#     }
-#   }
-# }
-
-# resource "aws_s3_bucket_logging" "example" {
-#   bucket = aws_s3_bucket.ci-example.id
-
-#   target_bucket = aws_s3_bucket.log_bucket.id
-#   target_prefix = "log/"
-# }
+resource "aws_s3_bucket" "my_bucket" {
+  #checkov:skip=CKV_AWS_18:Ensure the S3 bucket has access logging enabled
+  #checkov:skip=CKV2_AWS_61:Ensure that an S3 bucket has a lifecycle configuration
+  #checkov:skip=CKV2_AWS_62:Ensure S3 buckets should have event notifications enabled
+  #checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
+  #checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
+  #checkov:skip=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
+  #checkov:skip=CKV_AWS_21:Ensure all data stored in the S3 bucket have versioning enabled
+  bucket        = "ce7-ty-simple-ci"
+  force_destroy = true
+}
