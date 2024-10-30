646516adce003d7f0414b2d28497c10a0da57ef0
CyuanT <tan.yuan.cy@gmail.com>
2024-10-30 20:59:37 +0800

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,58 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "my_bucket" {
+resource "aws_s3_bucket" "ce7-ty-ci" {
   bucket        = "ce7-ty-simple-ci"
   force_destroy = true
+
+  lifecycle_rule {
+    id      = "expire"
+    status  = "Enabled"
+    prefix  = "logs/"
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+    expiration {
+      days = 90
+    }
+  }
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
+  bucket = aws_s3_bucket.ce7-ty-ci.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+  #checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
+}
+
+resource "aws_s3_bucket_logging" "example" {
+  bucket = aws_s3_bucket.ce7-ty-ci.id
+
+  target_bucket = aws_s3_bucket.log_bucket.id
+  target_prefix = "log/"
+}
+
+resource "aws_sns_topic" "bucket_notifications" {
+  name = "ce7-ty-ci-notifications"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.ce7-ty-ci.id
+
+  topic {
+    topic_arn     = aws_sns_topic.bucket_notifications.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_prefix = "logs/"
+  }
 }
