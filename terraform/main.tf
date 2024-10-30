d6b49f04195061384d42253772dda4a4e8d4ec8a
CyuanT <tan.yuan.cy@gmail.com>
2024-10-30 21:05:27 +0800

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -44,6 +44,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
   #checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
   #checkov:skip=CKV_AWS_144:Ensure that S3 bucket has cross-region replication enabled
   #checkov:skip=CKV_AWS_145:Ensure that S3 buckets are encrypted with KMS by default
+  #checkov:skip=CKV_AWS_26:Ensure all data stored in the SNS topic is encrypted
 }
 
 resource "aws_s3_bucket_logging" "example" {
@@ -64,5 +65,13 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
     topic_arn     = aws_sns_topic.bucket_notifications.arn
     events        = ["s3:ObjectCreated:*"]
     filter_prefix = "logs/"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "example" {
+  bucket = aws_s3_bucket.ce7-ty-ci.id
+
+  versioning_configuration {
+    status = "Enabled"
   }
 }
