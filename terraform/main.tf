0a051143179b7d88cb8c60bb51f50d79da856539
CyuanT <tan.yuan.cy@gmail.com>
2024-10-30 21:25:15 +0800

    add s3 bucket

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,8 +14,8 @@ resource "aws_s3_bucket" "ce7-ty-ci" {
   force_destroy = true
 
   lifecycle_rule {
+    enabled = true
     id     = "expire"
-    status = "Enabled"
     prefix = "logs/"
     transition {
       days          = 30
@@ -29,16 +29,6 @@ resource "aws_s3_bucket" "ce7-ty-ci" {
   versioning_configuration {
     status = "Enabled"
   }
-
-  resource "aws_s3_bucket_server_side_encryption_configuration" "good_sse_1" {
-    bucket = aws_s3_bucket.ce7-ty-ci.bucket
-
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.mykey.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
   }
 
   #checkov:skip=CKV2_AWS_6:Ensure that S3 bucket has a Public Access block
